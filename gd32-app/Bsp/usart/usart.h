c289e9b2303f276c9bd0ec621217fb9459fa6580
Deadline039 <1315374252@qq.com>
2026-09-15 00:21:22 +0800

    feat: add DEBUG option

--- gd32-app/Bsp/usart/usart.h
+++ gd32-app/Bsp/usart/usart.h
@@ -8,6 +8,8 @@
 #define USART_H
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,7 +19,15 @@ void usart0_init(void);
 
 void usart0_send(void *data, size_t len);
 size_t usart0_receive(void *buf, size_t buf_size);
-int usart0_printf(const char* fmt, ...);
+int usart0_printf(const char *fmt, ...);
+
+#ifdef DEBUG
+#define USART0_DEBUG_READ(_buf, _size) usart0_receive((_buf), (_size))
+#define USART0_DEBUG_PRINTF(...)       usart0_printf(__VA_ARGS__)
+#else /* DEBUG */
+#define USART0_DEBUG_READ(_buf, _size)
+#define USART0_DEBUG_PRINTF(...)
+#endif /* DEBUG */
 
 #ifdef __cplusplus
 }
